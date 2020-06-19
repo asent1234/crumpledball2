bd3599ffda8d3ebdf7df4b5a26a4599f33b5d803
asent1234 <42815464+asent1234@users.noreply.github.com>
2020-06-18 19:55:13 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+var ground, gameState
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+        gameState = "start"
+	engine = Engine.create();
+	world = engine.world
+
+	dustbin = new DustBin(720, 690, 100, 10)
+	paper = new Paper(100,600,10)
+  //Create a Ground
+  
+  ground = Bodies.rectangle(width/2, 700, width, 10 , {isStatic:true} );
+  
+ 	World.add(world, ground);
+    keyPressed();
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  if(gameState === "start"){
+  text("This is small game that will teach you the importance of throwing away your trash. Press Up Arrow to Start, and Up to throw away the trash.", 50,350)
+  if(keyCode === UP_ARROW){
+  gameState = "play"
+  }
+  }
+  if(gameState === "play"){
+  rectMode(CENTER);
+  background("white");
+  createEdgeSprites();
+  dustbin.display();
+  paper.display();
+  rect(ground.position.x, ground.position.y, width, 10)
+  }
+}
+
+
+function keyPressed() {
+ if (keyCode === UP_ARROW && gameState === "play") {
+    Matter.Body.applyForce(paper.body, paper.body.position, {x:12, y:-15});
+  }
+}
+
